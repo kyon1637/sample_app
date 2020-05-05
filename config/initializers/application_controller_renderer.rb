9853ce687d56888a78236c6862f9643fdaf22ea9
Taya Kyouhei <taya_mary919@icloud.com>
2020-05-05 18:34:08 +0900

    practice

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'daafc5ca591c7b4d5fe822e296350c09d192c4f1d0ad949d85112fadbb7d73c79bdbc8e9434b23e0255ea5c432229f50a1bfb5fe09302e5dec7a7385f903f907'
